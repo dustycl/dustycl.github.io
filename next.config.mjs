2b857b4f00660d9940ba02f4c5365b54323d726a
Dustin Lamperts <dustycl@gmail.com>
2024-06-19 17:42:24 -0700

    Publish site to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/dustycl.github.io' : '',
+  assetPrefix: isProd ? '/dustycl.github.io/' : '',
+  
+};
+
 
 export default nextConfig;
